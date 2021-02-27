626dfa38fd0a7d37f5b5c9ac107c6d1008e5fb85
vale2708 <valenpm99082@gmail.com>
2021-02-27 16:33:49 -0500

    atributo nombre
    creacion del atributo nombre con sus metodos get y set

--- modelo/Tarjeta.java
+++ modelo/Tarjeta.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
 /**
@@ -10,5 +6,15 @@ package modelo;
  * @author sagov8
  */
 public class Tarjeta {
-    
+     private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+     
+     
 }
