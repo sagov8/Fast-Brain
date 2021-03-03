9b87e62bdc06819d4e4570eeb16377cb838d8451
Santiago Vivas <60908806+sagov8@users.noreply.github.com>
2021-03-02 22:50:52 -0500

    Imágenes aleatorias
    Agregado método para obtener imágenes aleatorias(por probar)

--- modelo/Imagen.java
+++ modelo/Imagen.java
@@ -1,9 +1,14 @@
 package modelo;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
 import javax.swing.Icon;
 import javax.swing.ImageIcon; //cargar imagenes
 import java.util.Timer;
 import java.util.TimerTask; //el tiempo de ejecuccion
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import vista.Vista_Imagenes;
 
 public class Imagen {
@@ -13,7 +18,9 @@ public class Imagen {
     public static void main(String[] args) {
 
         int velocidad = 10; //segundos
-
+        List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4);
+        Collections.shuffle(numbers);
+        System.out.println(((Object) numbers).getClass().getSimpleName());
         Timer timer; //instancia
         TimerTask ejecutar;
 
@@ -29,28 +36,36 @@ public class Imagen {
 
                 Icon imagen; //instancia de imagen 
 
-                switch (contador) {
+                for (Integer numero : numbers) {
+                    contador = numero;
+                    switch (contador) {
 
-                    case 0:
-                        contador = 1;
-                        imagen = new ImageIcon(getClass().getResource("/imagenes/autor.png")); //se declara la imagen
-                        imagenes.Imgen_botero.setIcon(imagen); // de la instancia imagenes se cambia el jlabel
-                        break;
-                        
+                        case 0:
+                            contador = 1;
+                            imagen = new ImageIcon(getClass().getResource("/imagenes/autor.png")); //se declara la imagen
+                            imagenes.Imgen_botero.setIcon(imagen); // de la instancia imagenes se cambia el jlabel
+                            break;
+
                         case 1:
-                        contador = 2;
-                        imagen = new ImageIcon(getClass().getResource("/imagenes/maravillas.png")); //se declara la imagen
-                        imagenes.Imgen_botero.setIcon(imagen); // de la instancia imagenes se cambia el jlabel
-                        break;
-                        
+                            contador = 2;
+                            imagen = new ImageIcon(getClass().getResource("/imagenes/maravillas.png")); //se declara la imagen
+                            imagenes.Imgen_botero.setIcon(imagen); // de la instancia imagenes se cambia el jlabel
+                            break;
+                        default:
+                            System.out.println("No se pudo cargar imagen");
+                    }
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Imagen.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
 
             }
         };
-        
-       timer= new Timer();
-       timer.scheduleAtFixedRate(ejecutar, null, velmil); //peridodo de repeticion
-       
+
+        timer = new Timer();
+        timer.scheduleAtFixedRate(ejecutar, null, velmil); //peridodo de repeticion
 
     }
 
